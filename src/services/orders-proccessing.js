6ad8455d5e5256c8331c1a5915dff4683a2b3672
Andrey Fadeyev <fadeyeval@outlook.com>
2024-01-23 19:15:46 +0600

    fix: bun price must be twiced

--- src/services/orders-proccessing.js
+++ src/services/orders-proccessing.js
@@ -1,7 +1,7 @@
 
 export const getNomalizedOrderData = (order, ingredients) => {
   const orderIngredients = order.ingredients.flatMap(id => id ? ingredients.find(item => item._id === id) : []);
-  const orderPrice = orderIngredients.reduce((price, item) => price + item.price, 0);
+  const orderPrice = orderIngredients.reduce((price, item) => price + item.price * (item.type !== 'bun' ? 1 : 2), 0);
   const orderIngredientsCount = [...new Set(orderIngredients)].map(element => [element, orderIngredients.filter(item => item === element).length]);
   return {
     orderPrice,
