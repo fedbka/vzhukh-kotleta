d98454db245e34c07fae87b7d70e2db3c32d44aa
Andrey Fadeyev <fadeyeval@outlook.com>
2024-02-03 21:49:07 +0600

    fixes

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,19 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import App from "./components/app/app";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { store } from "./services/store"
+import { store } from "./services/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  //<React.StrictMode>
   <Provider store={store}>
-    <HashRouter basename="/">
+    <HashRouter>
       <App />
     </HashRouter>
   </Provider>
-  //</React.StrictMode>
 );
 
 reportWebVitals();
